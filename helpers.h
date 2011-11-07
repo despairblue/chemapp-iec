4e619bc0d76a344a56a032fee58656bead0569a6
Danny Arnold <danny.arnold@student.tu-freiberg.de>
2011-11-07 20:43:59 +0100

    removed deprecated stuff

--- helpers.h
+++ helpers.h
@@ -21,8 +21,6 @@ void show_amounts(DB[]);
 void enter_all_phases();
 void reset_vars(DB*, DB*, DB*, DB*, DB*, DB*);
 void set_all_ia(int[], int, int*);
-// int contains(int**, DB, DB, DB, int, int);
-// void add(int**, DB, DB, DB, int, int);
 void eliminate_phases(int[]);
 int check_for_ignored_element(int, int*);
 int sum_array(int[], int);
